4f4e922573956e3ab05c63c9f5f05508a22b0931
solinguo <552002499@qq.com>
2017-04-26 21:40:37 +0800

    Add rec time out

--- common/xmodem.c
+++ common/xmodem.c
@@ -13,7 +13,7 @@ date:2017-04-18
 #define NAK	0x15
 #define CAN	0x18
 
-#define NAK_CONT	10
+#define NAK_CONT	5
 unsigned char xmodem_buff[132];
 int xmodem_cont;
 
@@ -25,7 +25,9 @@ void xmodem(void)
 	
 	xmodem_cont = 0;	
 
+	printf("\n\rWait Received Data\n\r");
 	while(try--) {
+		printf("Wait %d s\r", try * 3);
 		fputc(NAK);
 		set_time_out(3000);
 		while(get_time_out()) {
@@ -46,24 +48,30 @@ void xmodem(void)
 			}
 		}
 	}
+	printf("\n\rNot received SOH,Try cont out \n\r");
 	return ;
 
 Rec_data:
 	while(1) {
-		while(xmodem_cont < 132) {
+		set_time_out(1000);
+		while((xmodem_cont < 132 ) && get_time_out()) {
 			xmodem_cont += uart_get_buff(&(xmodem_buff[xmodem_cont]));
 		}
 		fputc(ACK);
 		xmodem_cont = 0;
-		//TODO: copy data to flash
+		//TODO: Copy data to flash
 		memset(xmodem_buff, 0, 132);
-		while(xmodem_cont == 0)
+		while((xmodem_cont == 0) && get_time_out())
 			xmodem_cont += uart_get_buff(&(xmodem_buff[xmodem_cont]));
 		
 		if(xmodem_buff[0] == EOT)
 			break;
+		if(get_time_out() <= 0) {
+			printf("\n\rReceived Time Out\n\r");
+			return ;
+		}
 	}
-	//TODO: end transport
+	//TODO: End transport
 	fputc(ACK);
 	xmodem_cont = 0;
 
@@ -71,6 +79,8 @@ Rec_data:
 		xmodem_cont += uart_get_buff(&(xmodem_buff[xmodem_cont]));
 	
 	fputc(ACK);
+
+	printf("\n\rReceived Done\n\r");
 }
 
 
